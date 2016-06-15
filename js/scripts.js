2085a0742775da3a7d1cc8ab9483bd9094be34e8
Alex <alex@sitepoint.com>
2016-06-15 22:27:05 +1000

    Base HTML, CSS & JS

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// `element` is the element you want to wrap
+function dimension() {
+    var element = document.querySelectorAll(".view3d .cover");
+
+    for (var i = element.length - 1; i >= 0; i--) {
+        var parent = element[i].parentNode;
+        var img = element[i].querySelector(".cover img");
+        var workingcover = "background-image:url(" + img.src + ")"; // write it into some BG css
+        var bookgroup = document.createElement('div');
+        bookgroup.setAttribute("class", "bookgroup"); // set class
+        // set the wrapper as child (instead of the element)
+        var backcover = document.createElement('div');
+        backcover.setAttribute("class", "backcover"); // set class
+        backcover.setAttribute("style", workingcover);
+        // set the wrapper as child (instead of the element)
+        parent.replaceChild(bookgroup, element[i]);
+        // set element as child of wrapper
+
+        bookgroup.appendChild(backcover);
+        bookgroup.appendChild(element[i]);
+        //bookgroup.appendChild(color);
+    }
+}
+dimension();
