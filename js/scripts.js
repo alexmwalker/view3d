6a2dc99df6d38d11c7388f2dde690bb7ca428dab
Alex <alex@sitepoint.com>
2016-07-14 08:13:42 +0700

    Tiling error fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 // `element` is the element you want to wrap
+
+var lightingeffect = "url(../img/lighting.png)";
+
 function dimension() {
     var element = document.querySelectorAll(".view3d .cover");
 
     for (var i = element.length - 1; i >= 0; i--) {
         var parent = element[i].parentNode;
-        var img = element[i].querySelector(".cover img");
-        var workingcover = "background-image:url(" + img.src + ")"; // write it into some BG css
+        var img = element[i].querySelector(".cover img"); // grab the book IMG src
+
+        var workingcover = "background-image: " + lightingeffect+", url(" + img.src + ")" ; // write it into some BG css
         var bookgroup = document.createElement('div');
         bookgroup.setAttribute("class", "bookgroup"); // set class
         // set the wrapper as child (instead of the element)
