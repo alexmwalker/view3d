af14374d88c5b0e0f0910b62215aecb93304ebf7
Alex <alex@sitepoint.com>
2017-10-27 19:33:25 +1100

    Nailed it!
    Got the light-sheen reflection effect working on all sides - and in
    sync too.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,24 @@
 // `element` is the element you want to wrap
 
-var lightingeffect = "url(https://raw.githubusercontent.com/alexmwalker/view3d/master/img/lighting.png)";
+//var lightingeffect = "url(https://raw.githubusercontent.com/alexmwalker/view3d/master/img/lighting.png)";
+var LEgradient = "linear-gradient(100deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0.2) 25%, rgba(0,0,0,0.25) 50%, rgba(255,255,255,0) 100%)";
 
-function dimension() {
+function buildStructure() {
     var element = document.querySelectorAll(".view3d .cover");
 
     for (var i = element.length - 1; i >= 0; i--) {
         var parent = element[i].parentNode;
         var img = element[i].querySelector(".cover img"); // grab the book IMG src
 
-        var workingcover = "background-image: " + lightingeffect+", url(" + img.src + ")" ; // write it into some BG css
+        var backcoverBG = "background-image: " + LEgradient + ", url(" + img.src + ")" ; // write it into some BG css
+
+
         var bookgroup = document.createElement('div');
         bookgroup.setAttribute("class", "bookgroup"); // set class
         // set the wrapper as child (instead of the element)
         var backcover = document.createElement('div');
         backcover.setAttribute("class", "backcover"); // set class
-        backcover.setAttribute("style", workingcover);
+        backcover.setAttribute("style", backcoverBG);
         // set the wrapper as child (instead of the element)
         parent.replaceChild(bookgroup, element[i]);
         // set element as child of wrapper
@@ -25,4 +28,4 @@ function dimension() {
         //bookgroup.appendChild(color);
     }
 }
-dimension();
+buildStructure();
