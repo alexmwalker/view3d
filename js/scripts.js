2419e748f8f6c18a7d77d9ddcf0577a5959c8eff
Alex <alex@sitepoint.com>
2018-10-12 16:55:59 +1100

    Added comment containing server location:
    https://cdn.staticaly.com/gh/alexmwalker/view3d/master/js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 // `element` is the element you want to wrap
 
+// served: https://cdn.staticaly.com/gh/alexmwalker/view3d/master/js/scripts.js
+
 //var lightingeffect = "url(https://raw.githubusercontent.com/alexmwalker/view3d/master/img/lighting.png)";
 var LEgradient = "linear-gradient(100deg, rgba(255,255,255,0) 0%, rgba(255,255,255,0.2) 25%, rgba(0,0,0,0.25) 50%, rgba(255,255,255,0) 100%)";
 
